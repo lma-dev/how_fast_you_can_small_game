f6b5f600c3899c68b7fd41bc01987acb99594958
lwinmoeaung-springarts <blackplayerbk@gmail.com>
2022-03-21 23:35:21 +0630

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,54 @@
 <template>
   <h1>How fast can you catch me?</h1>
-  <button
-    @click="start"
-    :disabled="isPlaying"
-  >Play</button>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <h4 @showBlock="changeText">{{ Text }}</h4>
   <div v-if="isPlaying">
-    <Block :delay="delay" />
+    <Block :delay="delay" @endGame="endGame" />
   </div>
-
+  <div v-if="end">
+    <Result :score="score" />
+  </div>
 </template>
 
 <script>
-import Block from './components/Block.vue'
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Block
+    Block,
+    Result,
   },
   data() {
     return {
       isPlaying: false,
-      delay: null
-    }
+      delay: null,
+      score: 0,
+      end: false,
+      Text: null,
+    };
   },
   methods: {
     start() {
+      this.Text = "Loading ......";
       this.isPlaying = true;
       this.delay = 2000 + Math.random() * 5000;
-    }
-  }
-}
+    },
+    changeText() {
+      this.Text = "Catch Now!!!";
+    },
+    endGame(score) {
+      this.score = score;
+      this.isPlaying = false;
+      this.end = true;
+      this.Text = "";
+    },
+  },
+};
 </script>
 
-<style >
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,29 +1,39 @@
 <template>
-  <div
-    class="block"
-    v-if="showBlock"
-  >
-    Click Here!!!
-  </div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Here!!!</div>
 </template>
 
 <script>
 export default {
-  props: ['delay'],
+  props: ["delay"],
   data() {
     return {
-      showBlock: false
-    }
+      showBlock: false,
+      score: 0,
+      timer: null,
+    };
   },
   mounted() {
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer();
     }, this.delay);
-  }
-}
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.score += 50;
+      }, 50);
+      this.$emit("showBlock");
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("endGame", this.score);
+    },
+  },
+};
 </script>
 
-<style >
+<style>
 .block {
   background-color: aqua;
   width: 400px;
